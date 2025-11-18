73ff2fd1e4221d3520c0a88a623cbfa069e8bf67
donheekang <148280996+donheekang@users.noreply.github.com>
2025-11-19 06:59:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -262,4 +262,4 @@ async def upload_cert_pdf(
 # ------------------------------------------------------
 if _name_ == "_main_":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
